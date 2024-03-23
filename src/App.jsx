a20de1ae8d25a11df41bfd43745df3c6a8ed7204
Faisal Ahmed Fahim <146206158+fahim-ahmed10@users.noreply.github.com>
2024-03-24 00:34:15 +0600

    Navbar completed with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      
-      <h1 className='text-3xl bg-red-400'>Chefs Table</h1>
-      
+    <>      
+  <Navbar></Navbar>
+  <Hero></Hero>
+            
     </>
   )
 }
