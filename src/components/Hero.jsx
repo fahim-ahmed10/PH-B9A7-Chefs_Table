800ffe2695ff9f5e49a02d6fe4fe8cc16d805154
Faisal Ahmed Fahim <146206158+fahim-ahmed10@users.noreply.github.com>
2024-03-24 01:26:04 +0600

    hero section done with responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,26 @@
-import heroImage from '../assets/Rectangle 1.png';
+import heroImage from "../assets/hero-image.png";
 const Hero = () => {
   return (
-    <div className="container mx-auto ">
+    <div className="container mx-auto mt-5 px-2">
       <div
-        className="hero min-h-screen"
+        className="hero h-[600px] rounded-2xl"
         style={{
           backgroundImage:
-            {heroImage},
+            `url(${heroImage})`,
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+        <div className="hero-content text-center">
+          <div className="">
+            <h1 className="mb-7 text-2xl md:text-4xl lg:text-5xl font-bold text-white">Discover an exceptional cooking <br/> class tailored for you</h1>
+            <p className="mb-10 text-white text-sm md:text-lg">
+                Learn and Master Basic Programming,  Data Structures, Algorithm, OOP, Database and solve 500+ coding 
+                problems to become an exceptionally well world-class Programmer.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <div className="flex justify-center gap-4 md:gap-5">
+            <button className="btn bg-[#0be58a] hover:bg-[#76fdc4] border-none rounded-3xl px-3 md:px-6 text-lg font-bold">Explore Now</button>
+            <button className="border rounded-3xl text-lg hover:text-[#0be58a] font-bold px-3 md:px-6 text-white">Our Feedback</button>
+            </div>
+
           </div>
         </div>
       </div>
