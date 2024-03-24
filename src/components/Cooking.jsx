347c670194920c15bd05ec42100d666fe6262ad2
Faisal Ahmed Fahim <146206158+fahim-ahmed10@users.noreply.github.com>
2024-03-24 14:02:52 +0600

    our recipe section done with responsive

--- src/components/Cooking.jsx
+++ src/components/Cooking.jsx
@@ -3,8 +3,8 @@ import clockIcon from '../assets/icons8-clock-32.png';
 import fireIcon from '../assets/icons8-fire-50.png';
 const Cooking = () => {
     return (
-        <div className='container mx-auto mt-5 px-2 flex md:gap-5 lg:gap-10'>
-            <div className="md:w-[50%] lg:w-[65%] grid md:grid-cols-1 lg:grid-cols-2 md:gap-10 lg:gap-3">
+        <div className='container mx-auto mt-5 px-2 flex flex-col md:flex-col lg:flex-row gap-10 md:gap-5 lg:gap-10'>
+            <div className="md:w-[50%] lg:w-[65%] grid md:grid-cols-1 lg:grid-cols-2 self-center gap-5 md:gap-10 lg:gap-3">
                 <div className='w-[379px] border p-6 rounded-2xl'>
                     <div>
                         <img src={bolognesePic} alt="bolognese" />
@@ -29,7 +29,7 @@ const Cooking = () => {
                         <img className='h-5' src={fireIcon} alt="fire" />
                         <h4>600 calories</h4>
                     </div>
-                    <button className='text-xl font-bold bg-[#0be58a] px-5 py-3 rounded-3xl'>Want to Cook</button>
+                    <button className='text-xl font-bold bg-[#0be58a] hover:bg-[#6cfbbf] px-5 py-3 rounded-3xl'>Want to Cook</button>
                 </div>
                 <div className='w-[379px] border p-6 rounded-2xl'>
                     <div>
@@ -55,19 +55,19 @@ const Cooking = () => {
                         <img className='h-5' src={fireIcon} alt="fire" />
                         <h4>600 calories</h4>
                     </div>
-                    <button className='text-xl font-bold bg-[#0be58a] px-5 py-3 rounded-3xl'>Want to Cook</button>
+                    <button className='text-xl font-bold bg-[#0be58a] hover:bg-[#6cfbbf] px-5 py-3 rounded-3xl'>Want to Cook</button>
                 </div>
             </div>
-            <div className="md:w-[45%] lg:w-[35%]">
+            <div className="w-full md:w-[45%] lg:w-[35%] mx-auto">
                 <div className='w-full border rounded-2xl'>
-                    <h1 className='text-center font-bold '>Want to cook: 01</h1>
+                    <h1 className='text-center font-bold mt-5'>Want to cook: 01</h1>
                     <hr className='mt-3 w-[70%] mx-auto' />
                     <div>
                         <table className='mt-3'>
                             <tr>
                                 <td></td>
-                                <td className='pb-4 pl-3 md:pr-10 lg:pr-20 w-32 text-start'>Name</td>
-                                <td className='pb-4 w-20 text-start'>Time</td>
+                                <td className='pb-4 pl-3 md:pr-10 lg:pr-20 w-[35%] lg:w-32 text-start'>Name</td>
+                                <td className='pb-4 w-[25%] lg:w-20 text-start'>Time</td>
                                 <td className='pb-4 w-20 text-start'>Calories</td>
                                 <td></td>
                             </tr>
@@ -80,14 +80,14 @@ const Cooking = () => {
                             </tr>
                         </table>
                     </div>
-                    <h1 className='text-center font-bold '>Currently cooking: 02</h1>
+                    <h1 className='text-center font-bold mt-6'>Currently cooking: 02</h1>
                     <hr className='mt-3 w-[70%] mx-auto' />
-                    <div>
+                    <div className='mb-5'>
                         <table className='mt-3'>
                             <tr>
                                 <td></td>
-                                <td className='pb-4 pl-3 md:pr-20 lg:pr-28 w-32 text-start'>Name</td>
-                                <td className='pb-4 md:pr-10 lg:pr-20 w-20 text-start'>Time</td>
+                                <td className='pb-4 pl-3 md:pr-20 lg:pr-28 w-[35%] lg:w-32 text-start'>Name</td>
+                                <td className='pb-4 md:pr-10 lg:pr-20 w-[25%] lg:w-20 text-start'>Time</td>
                                 <td className='pb-4 w-full text-start'>Calories</td>
                             </tr>
                             <tr className='bg-[#f9f9f9] text-sm'>
@@ -102,9 +102,14 @@ const Cooking = () => {
                                 <td className='pr-4 pt-4'>20 Minutes</td>
                                 <td className='pr-4 pt-4'>400 calories</td>
                             </tr>
+                            <tr className='text-sm'>
+                                <td className='pl-3 pt-4'></td>
+                                <td className='pl-3 pr-4 pt-4'></td>
+                                <td className='pr-4 pt-4'>Total Time = <br />45 minutes</td>
+                                <td className='pr-4 pt-4'>Total calories = <br /> 400 calories</td>
+                            </tr>
                         </table>
                     </div>
-                    
                 </div>
             </div>
         </div>
